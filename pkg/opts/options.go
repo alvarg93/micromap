7f29b767e19a38d4d8dcb4510299faf68de75e37
Łukasz Janyga <lukaszjanyga@C02XX1Q6JGH5.local>
2019-03-03 18:39:48 -0600

    fix naming, fix opts, update gitignore

--- pkg/opts/options.go
+++ pkg/opts/options.go
@@ -6,16 +6,15 @@ import (
 )
 
 var help = `
-
 Micromap generates a dependency graph from code annotations.
-
 Usage: micromap [-x=val][-r][-d=val][-i=val][-f=val]
-  -h, --help 				- this message
-	-x, --regex 			- regex pattern of files to scan
-	-r, --recursive 	- scan recursive
-	-d, --dot 				- output file for a dot graph
-	-i, --img 				- output file for visual representation of the graph
-	-f, --format 			- format of the image file
+
+  -h, --help        - this message
+  -x, --regex       - regex pattern of files to scan
+  -r, --recursive   - scan recursive
+  -d, --dot         - output file for a dot graph
+  -i, --img         - output file for visual representation of the graph
+  -f, --format      - format of the image file
 `
 
 //Options represent command line arguments for the program
@@ -42,23 +41,17 @@ func ParseArgs(args []string) (opts Options, h bool) {
 	for _, arg := range args {
 		optVal := strings.Split(arg, "=")
 		switch optVal[0] {
-		case "-x":
-		case "--regex":
+		case "-x", "--regex":
 			o.Regex = optVal[1]
-		case "-r":
-		case "--recursive":
+		case "-r", "--recursive":
 			o.Recursive = true
-		case "-d":
-		case "--dot":
+		case "-d", "--dot":
 			o.DotFile = optVal[1]
-		case "-i":
-		case "--img":
+		case "-i", "--img":
 			o.ImgFile = optVal[1]
-		case "-f":
-		case "--format":
+		case "-f", "--format":
 			o.ImgFormat = optVal[1]
-		case "-h":
-		case "--help":
+		case "-h", "--help":
 			fmt.Print(help)
 			return o, true
 		default:
